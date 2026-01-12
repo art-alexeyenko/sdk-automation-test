b62c5f455b17e9aecd7abeab3a9019d46327999a
Syed Afiq Syed Itam <syedafiq.syeditam+sitecore@sitecore.com>
2026-01-12 18:53:41 +0800

    Update to 1.4.0-canary.14

--- examples/kit-nextjs-skate-park/src/app/[site]/[locale]/[[...path]]/page.tsx
+++ examples/kit-nextjs-skate-park/src/app/[site]/[locale]/[[...path]]/page.tsx
@@ -1,24 +1,14 @@
-import { isDesignLibraryPreviewData } from "@sitecore-content-sdk/nextjs/editing";
-import { notFound } from "next/navigation";
-import { draftMode } from "next/headers";
-import { SiteInfo } from "@sitecore-content-sdk/nextjs";
-import sites from ".sitecore/sites.json";
-import { routing } from "src/i18n/routing";
-import scConfig from "sitecore.config";
-import client from "src/lib/sitecore-client";
-import Layout, { RouteFields } from "src/Layout";
-import components from ".sitecore/component-map";
-import Providers from "src/Providers";
-import { NextIntlClientProvider } from "next-intl";
-import { setRequestLocale } from "next-intl/server";
+import { isDesignLibraryPreviewData } from '@sitecore-content-sdk/nextjs/editing';
+import { notFound } from 'next/navigation';
+import { draftMode } from 'next/headers'
+import client from 'src/lib/sitecore-client';
+import Layout, { RouteFields } from 'src/Layout';
+import Providers from 'src/Providers';
+import { NextIntlClientProvider } from 'next-intl';
+import { setRequestLocale } from 'next-intl/server';
 
 type PageProps = {
-  params: Promise<{
-    site: string;
-    locale: string;
-    path?: string[];
-    [key: string]: string | string[] | undefined;
-  }>;
+  params: Promise<{ site: string; locale: string; path?: string[]; [key: string]: string | string[] | undefined }>;
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
@@ -47,42 +37,14 @@ export default async function Page({ params, searchParams }: PageProps) {
     notFound();
   }
 
-  // Fetch the component data from Sitecore (Likely will be deprecated)
-  const componentProps = await client.getComponentData(
-    page.layout,
-    {},
-    components
-  );
-
   return (
     <NextIntlClientProvider>
-      <Providers page={page} componentProps={componentProps}>
+      <Providers page={page}>
         <Layout page={page} />
       </Providers>
     </NextIntlClientProvider>
   );
 }
-
-// This function gets called at build and export time to determine
-// pages for SSG ("paths", as tokenized array).
-export const generateStaticParams = async () => {
-  if (process.env.NODE_ENV !== "development" && scConfig.generateStaticPaths) {
-    // Filter sites to only include the sites this starter is designed to serve.
-    // This prevents cross-site build errors when multiple starters share the same XM Cloud instance.
-    const defaultSite = scConfig.defaultSite;
-    const allowedSites = defaultSite
-      ? sites
-          .filter((site: SiteInfo) => site.name === defaultSite)
-          .map((site: SiteInfo) => site.name)
-      : sites.map((site: SiteInfo) => site.name);
-
-    return await client.getAppRouterStaticParams(
-      allowedSites,
-      routing.locales.slice()
-    );
-  }
-  return [];
-};
 
 // Metadata fields for the page.
 export const generateMetadata = async ({ params }: PageProps) => {
@@ -91,9 +53,6 @@ export const generateMetadata = async ({ params }: PageProps) => {
   // The same call as for rendering the page. Should be cached by default react behavior
   const page = await client.getPage(path ?? [], { site, locale });
   return {
-    title:
-      (
-        page?.layout.sitecore.route?.fields as RouteFields
-      )?.Title?.value?.toString() || "Page",
+    title: (page?.layout.sitecore.route?.fields as RouteFields)?.Title?.value?.toString() || 'Page',
   };
 };
