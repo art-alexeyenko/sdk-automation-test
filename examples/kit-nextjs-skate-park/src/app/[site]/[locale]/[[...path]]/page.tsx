37b6cd8ad55db0b95f4e7d74f307a4014494e98b
syed_sitecore <XMCtest@sitecore.com>
2026-04-27 13:05:59 +0000

    Scaffolded new Content SDK application version 2.0.2

--- examples/kit-nextjs-skate-park/src/app/[site]/[locale]/[[...path]]/page.tsx
+++ examples/kit-nextjs-skate-park/src/app/[site]/[locale]/[[...path]]/page.tsx
@@ -1,6 +1,6 @@
 import { isDesignLibraryPreviewData } from '@sitecore-content-sdk/nextjs/editing';
 import { notFound } from 'next/navigation';
-import { draftMode, headers as nextHeaders } from 'next/headers';
+import { draftMode } from 'next/headers';
 import { SiteInfo } from '@sitecore-content-sdk/nextjs';
 import sites from '.sitecore/sites.json';
 import { routing } from 'src/i18n/routing';
@@ -13,9 +13,10 @@ import { setRequestLocale } from 'next-intl/server';
 
 type PageProps = {
   params: Promise<{ site: string; locale: string; path?: string[]; [key: string]: string | string[] | undefined }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
-export default async function Page({ params }: PageProps) {
+export default async function Page({ params, searchParams }: PageProps) {
   const { site, locale, path } = await params;
 
   // Set site and locale to be available in src/i18n/request.ts for fetching the dictionary
@@ -26,13 +27,11 @@ export default async function Page({ params }: PageProps) {
   // Fetch the page data from Sitecore
   let page;
   if (draft.isEnabled) {
-    const headers = await nextHeaders();
-    const { previewData, fetchOptions } = client.getPreviewInputs(headers);
-
-    if (isDesignLibraryPreviewData(previewData)) {
-      page = await client.getDesignLibraryData(previewData, fetchOptions);
+    const editingParams = await searchParams;
+    if (isDesignLibraryPreviewData(editingParams)) {
+      page = await client.getDesignLibraryData(editingParams);
     } else {
-      page = await client.getPreview(previewData, fetchOptions);
+      page = await client.getPreview(editingParams);
     }
   } else {
     page = await client.getPage(path ?? [], { site, locale });
