1835cfbe9fc60fe35f20edc9ade36f7aae5013cb
Syed Afiq Syed Itam <syedafiq.syeditam+sitecore@sitecore.com>
2026-08-21 18:42:59 +0800

    Update rich-text components to support red variant

--- examples/angular/src/app/components/rich-text.component.ts
+++ examples/angular/src/app/components/rich-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed } from '@angular/core';
+import { Component, Directive, computed } from '@angular/core';
 import { ScRichTextDirective, TextField } from '@sitecore-content-sdk/angular';
 import { SxaComponent } from './content-sdk/sxa.component';
 
@@ -6,21 +6,38 @@ interface RichTextFields {
   Text?: TextField;
 }
 
+const RICH_TEXT_HOST = {
+  '[attr.class]': "('component rich-text ' + styles().trim())",
+  '[attr.id]': 'renderingId()',
+} as const;
+
+@Directive()
+abstract class RichTextBase extends SxaComponent {
+  readonly contentField = computed(() => (this.fields() as RichTextFields)?.Text);
+}
+
 @Component({
   selector: 'app-rich-text',
   imports: [ScRichTextDirective],
-  host: {
-    '[attr.class]': "('component rich-text ' + styles().trim())",
-    '[attr.id]': 'renderingId()',
-  },
+  host: RICH_TEXT_HOST,
   template: `
     <div class="component-content">
       <div *scRichText="contentField()"></div>
     </div>
   `,
 })
-export class RichTextComponent extends SxaComponent {
-  readonly contentField = computed(() => (this.fields() as RichTextFields)?.Text);
-}
+class RichTextComponent extends RichTextBase {}
 
-export default RichTextComponent;
+@Component({
+  selector: 'app-rich-text-red',
+  imports: [ScRichTextDirective],
+  host: RICH_TEXT_HOST,
+  template: `
+    <div class="component-content">
+      <div class="text-red-600" *scRichText="contentField()"></div>
+    </div>
+  `,
+})
+class RichTextRedComponent extends RichTextBase {}
+
+export { RichTextComponent as Default, RichTextRedComponent as Red };
